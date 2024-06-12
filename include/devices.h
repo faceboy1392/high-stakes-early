afacb76c2edd38d05968e8fc8f011197a16d0284
faceboy <faceboy1392@gmail.com>
2024-06-12 11:23:19 -0500

    motors

--- include/devices.h
+++ include/devices.h
@@ -1,1 +1,11 @@
-export 
+#include "main.h"
+
+extern Controller master;
+extern Controller partner;
+
+extern Motor motor_l1,
+    motor_l2,
+    motor_l3,
+    motor_r1,
+    motor_r2,
+    motor_r3;
--- src/devices.cpp
+++ src/devices.cpp
@@ -2,7 +2,15 @@
 #include "lemlib/api.hpp"
 #include "pros/abstract_motor.hpp"
 #include "pros/misc.h"
+#include "pros/motors.h"
 
 Controller master(E_CONTROLLER_MASTER);
 Controller partner(E_CONTROLLER_PARTNER);
+
+Motor motor_l1(-18, MotorGearset::blue, MotorUnits::degrees),
+    motor_l2(-19, MotorGearset::blue, MotorUnits::degrees),
+    motor_l3(20, MotorGearset::blue, MotorUnits::degrees),
+    motor_r1(8, MotorGearset::blue, MotorUnits::degrees),
+    motor_r2(9, MotorGearset::blue, MotorUnits::degrees),
+    motor_r3(-10, MotorGearset::blue, MotorUnits::degrees);
 
